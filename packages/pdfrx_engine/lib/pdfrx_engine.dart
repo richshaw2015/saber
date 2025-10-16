f2f341fb0100e03ca5e0bbd35fea8248eb6ba909
Neo <richshaw@126.com>
2025-10-16 13:46:06 +0800

    remove pdfrx deps

--- packages/pdfrx_engine/lib/pdfrx_engine.dart
+++
@@ -1,5 +0,0 @@
-library;
-
-export 'src/mock/pdfrx_initialize_mock.dart' if (dart.library.io) 'src/pdfrx_initialize_dart.dart';
-export 'src/pdfrx_api.dart';
-export 'src/pdfrx_dart.dart';
--- packages/pdfrx_engine/lib/src/native/string_buffer_wrapper.dart
+++
@@ -1,8 +0,0 @@
-/// This is a workaround for WASM+Safari StringBuffer issue. For native code, use StringBuffer directly.
-typedef StringBufferWrapper = StringBuffer;
-
-/// This is a workaround for WASM+Safari StringBuffer issue (#483).
-///
-/// - for native code, use [StringBuffer] directly
-/// - for Flutter Web, use this [StringBufferWrapper] that internally uses [String] instead.
-StringBufferWrapper createStringBufferForWorkaroundSafariWasm() => StringBuffer();
