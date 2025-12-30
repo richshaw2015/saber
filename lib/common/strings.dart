0f4d1856451e7977facf12926c46938f35ffea4a
Neo <richshaw@126.com>
2025-12-30 16:17:30 +0800

    update app name

--- lib/common/strings.dart
+++ lib/common/strings.dart
@@ -1,5 +1,5 @@
 class Strings {
-  static const String appName = "Saber 手写笔记";
+  static const String appName = "手写飒记";
 
   static const String copyright = "Copyright © 2025 邢中祥 All rights reserved";
 }
